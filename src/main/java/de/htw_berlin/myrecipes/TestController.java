39a1a5b028504c6cde238dc35ca56634f397881a
Simar Keskinoglu <simar.ks@icloud.com>
2026-04-20 13:24:45 +0200

    Entity Class

--- src/main/java/de/htw_berlin/myrecipes/TestController.java
+++ src/main/java/de/htw_berlin/myrecipes/TestController.java
@@ -3,11 +3,18 @@ package de.htw_berlin.myrecipes;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
-    public String test() {
-        return "Tim und Simars Website";
+    @GetMapping("/recipes")
+    public List<Recipe> getAllRecipes() {
+        return List.of(
+                new Recipe("Lasagne"),
+                new Recipe("Yaglama"),
+                new Recipe("Pizza Diavola"),
+                new Recipe("Hausgemachter Döner")
+        );
     }
 }
